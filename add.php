c03ae1955ea6d7113aa07259a83648ae1399e128
Vinodh Kumar K <87415270+vinodh4@users.noreply.github.com>
2023-10-30 12:43:11 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,72 @@
+<?php
+   $connection = mysqli_connect("localhost","root","");
+    $db = mysqli_select_db($connection,"crud operation");
+
+    if(isset($_POST['submit']))
+        {
+          $name = $_POST['name'];
+          $address = $_POST['address'];
+         $mobile = $_POST['mobile'];
+
+           $sql = "insert into student(name,address,mobile)values(' $name',' $address',' $mobile')";
+
+           if(mysqli_query($connection,$sql))
+           {
+                  echo '<script> location.replace ("index.php")</script>';  
+           }
+           else
+           {
+           echo "Some thing Error" . $connection->error;
+           }
+        }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Crud Operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+
+        <div class="container">
+            <div class="row">
+                 <div class="col-md-9">
+                    <div class="card">
+                    <div class="card-header">
+                        <h1> Crud Operation </h1>
+                    </div>
+                    <div class="card-body">
+
+                    <form action="add.php" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control"  placeholder="Enter Name"> 
+                        </div>
+
+                        <div class="form-group">
+                            <label>Address</label>
+                            <input type="text" name="address" class="form-control"  placeholder="Enter Address"> 
+                        </div>
+
+                        <div class="form-group">
+                            <label>Mobile</label>
+                            <input type="number" name ="mobile" class="form-control"  placeholder="Enter Mobile"> 
+                        </div>
+                        <br/>
+                        <input type="submit" class="btn btn-primary" name="submit" value="Register">
+                        </form>
+                   
+                    </div>
+                    </div>
+
+                </div>
+            
+            </div>
+        </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+        <div class="container">
+            <div class="row">
+                 <div class="col-md-9">
+                    <div class="card">
+                    <div class="card-header">
+                        <h1>Crud Operation</h1>
+                    </div>
+                    <div class="card-body">
+                   
+                    <button class="btn btn-success"> <a href="add.php" class="text-light"> Add New </a> </button>
+                        
+                        <br/>
+                        <br/>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Mobile</th>
+                            <th scope="col">Option</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <?php
+                                $connection = mysqli_connect("localhost","root","");
+                                $db = mysqli_select_db($connection,"crud operation");
+
+                                $sql = "select * from student";
+                                $run = mysqli_query($connection, $sql);
+                                $id= 1;
+
+                                while($row = mysqli_fetch_array($run))
+                                {
+                                    $uid = $row['id'];
+                                    $name = $row['name'];
+                                    $address = $row['address'];
+                                    $mobile = $row['mobile'];
+                                ?>
+
+                                   <tr>
+                                        <td><?php echo $id ?></td>
+                                        <td><?php echo $name ?></td>
+                                        <td><?php echo $address ?></td>
+                                        <td><?php echo $mobile ?></td>
+
+                                        <td>
+                                        <button class="btn btn-success"> <a href='edit.php?edit=<?php echo $uid ?>' class="text-light"> Edit </a> </button> &nbsp;
+                                       <button class="btn btn-danger"><a href='delete.php?del=<?php echo $uid ?>' class="text-light"> Delete </a> </button>
+                                        </td>
+                                   </tr>
+                                    <?php $id++; } ?>
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
